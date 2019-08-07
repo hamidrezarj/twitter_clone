6231019eac56fa48ef880717100f3d176a99f2ef
Hamidreza <hamidranjbarpour771@gmail.com>
2019-08-07 20:15:23 +0430

    Creating Post and Comment Models

---
+++ tweets/models.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import User
+from django.db import models
+from datetime import timezone
+
+
+# Create your models here.
+class Post(models.Model):
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_liked = False
+
+    def __str__(self):
+        return self.content
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Likes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
