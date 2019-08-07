27fd3e9b3227e5517bbdc09ce1b6fea8560c76fe
Hamidreza <hamidranjbarpour771@gmail.com>
2019-08-07 20:13:34 +0430

    adding new URLs

---
+++ tweets/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+app_name = 'tweets'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('register/', views.register_view, name='register'),
+    path('post/', views.post, name='post'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('user/<username>/', views.personal_page, name='user'),
+    path('hashtag/<hashtag>/', views.hashtag_view, name='hashtag'),
+    path('comment/<int:post_id>', views.comment_view, name='comment'),
+
+]
