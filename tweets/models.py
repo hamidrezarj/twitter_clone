a61a6a3468e5ce20e512faded85e7b71485c9d33
hamidrezaRanjbarpour <53371031+hamidrezaRanjbarpour@users.noreply.github.com>
2021-01-24 22:42:34 +0330

    following & profile image added to model

--- tweets/models.py
+++ tweets/models.py
@@ -4,10 +4,20 @@ from datetime import timezone
 
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    following = models.ManyToManyField(User, related_name='following', blank=True)
+    profile_img = models.ImageField(upload_to='avatars', blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
 class Post(models.Model):
     content = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    media = models.FileField(upload_to='media', blank=True)
     is_liked = False
 
     def __str__(self):
@@ -17,17 +27,16 @@ class Post(models.Model):
 class Comment(models.Model):
     content = models.TextField()
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     pub_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 
     def __str__(self):
-        return self.user.username
+        return self.profile.user.username
 
 
 class Likes(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.user.username
-
+        return self.profile.user.username
